e4444aa0d4c626b342e9b6847d46598ca00643bb
Yahir León <leonperezyahir0@gmail.com>
2025-07-06 16:22:39 -0600

    prueba 1

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, Flask
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,10 +6,19 @@ app = Flask(__name__)
 def index():
     return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        user = request.form.get('username', '').strip()
+        pw = request.form.get('password', '')
+        if (user == "Materiales01" and pw == "Materiales2025") or \
+           (user == "1111" and pw == "1111"):
+            return redirect(url_for('material'))
+        return render_template('login.html', error="Usuario o contraseña incorrectos. Por favor, intente de nuevo")
+    
+    # ✅ Para método GET, simplemente mostrar el formulario:
     return render_template('login.html')
 
-@app.route('/material')
+@app.route('/Materiales')
 def material():
     return render_template('MaterialTemplate.html')
--- run.py
+++ run.py
@@ -2,3 +2,6 @@ from app.routes import app
 
 if __name__ == '__main__':
     app.run(host='192.168.1.18', port=5000)
+
+
+
