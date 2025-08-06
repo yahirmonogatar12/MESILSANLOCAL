09d7dccdd7894998081e3d368487047d132396ae
Yahir León <leonperezyahir0@gmail.com>
2025-08-06 08:07:52 -0600

    Limpieza de codigo

--- update_query.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content 'c:\Users\yahir\OneDrive\Escritorio\ISEMM_MES\app\smt_routes_fixed.py'
-$content[169] = '                PartName, Quantity, SEQ, Vendor,'
-$newContent = $content[0..169] + '                PreviousBarcode, Productdate, FeederBase' + $content[170..($content.Length-1)]
-$newContent | Set-Content 'c:\Users\yahir\OneDrive\Escritorio\ISEMM_MES\app\smt_routes_fixed.py'
