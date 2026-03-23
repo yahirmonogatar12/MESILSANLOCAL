f1fc68640e040cfe9915026f9cb743a005048f1b
yahirmonogatar12 <leonperezyahir0@gmail.com>
2026-03-23 16:30:15 -0600

    Compatibilidad Docker

--- run.py
+++ run.py
@@ -2,11 +2,10 @@ import os
 from waitress import serve
 from app_factory import create_app
 
-# En ejecución productiva con waitress no usamos reloader.
 os.environ.setdefault("MES_USE_RELOADER", "0")
-
 app = create_app()
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000, threads=8)
-    print("Servidor iniciado en http://0.0.0.0:5000")
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Servidor iniciado en http://0.0.0.0:{port}")
+    serve(app, host='0.0.0.0', port=port, threads=8)
