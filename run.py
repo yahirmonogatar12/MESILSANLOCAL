0dd521ffa90b7dce3978e98945e22d1159428e59
yahirmonogatar12 <129004736+yahirmonogatar12@users.noreply.github.com>
2026-04-23 10:30:39 -0600

    Test git2

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app_factory import create_app
 os.environ.setdefault("MES_USE_RELOADER", "0")
 app = create_app()
 
-f __name__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     print(f"Servidor iniciado en http://0.0.0.0:{port}")
     serve(app, host='0.0.0.0', port=port, threads=8)
