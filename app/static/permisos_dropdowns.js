ede4a207b7acea64f8a650bac26d72405fa424e8
ChaiGmzR <jesus.cop11@gmail.com>
2026-07-28 14:19:07 -0600

    Integracion de modulos hisotrial FCT 28072026.3

--- app/static/permisos_dropdowns.js
+++ app/static/permisos_dropdowns.js
@@ -362,6 +362,16 @@ const allDropdowns = [
     {
         "pagina": "LISTA_DE_CONTROL_DE_RESULTADOS",
         "seccion": "Historial de maquinas calidad",
+        "boton": "Historial de maquina FCT"
+    },
+    {
+        "pagina": "LISTA_DE_CONTROL_DE_RESULTADOS",
+        "seccion": "Historial de maquinas calidad",
+        "boton": "Historial de maquina FCT % Pass/Fail"
+    },
+    {
+        "pagina": "LISTA_DE_CONTROL_DE_RESULTADOS",
+        "seccion": "Historial de maquinas calidad",
         "boton": "Historial de maquina vision"
     },
     {
